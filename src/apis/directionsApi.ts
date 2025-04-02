645684cd6d414c17dbf285ed60f3a948fe22c0fd
Datakokk <fiodorelzarster@gmx.com>
2025-04-02 19:18:37 +0200

    feat(docker): add default Nginx config to support Cloud Run and SPA routing

--- src/apis/directionsApi.ts
+++ src/apis/directionsApi.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 const directionsApi = axios.create({
-    baseURL: 'https://api.mapbox.com/directions/v5/mapbox/driving',
-    params: {
-        alternatives: true,
-        geometries: 'geojson',
-        overview: 'simplified',
-        steps: false,
-        access_token: 'pk.eyJ1IjoiZGF0YWtva2siLCJhIjoiY2xmOXhpa29oMHN0bjN0bzJlaWlsOG1ldiJ9.8zXIGNj38SWjiHtWn5WOWw'
-    }
+  baseURL: "https://api.mapbox.com/directions/v5/mapbox/driving",
+  params: {
+    alternatives: true,
+    geometries: "geojson",
+    overview: "simplified",
+    steps: false,
+    access_token:
+      "pk.eyJ1IjoiZGF0YWtva2siLCJhIjoiY205MDZ4OTU4MGtpdTJyc2R0dzNhMWU5bSJ9.uDPETX3L-_Gm4F6_vL83AA",
+  },
 });
 
 export default directionsApi;
